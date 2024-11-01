af259461c55020f3bc3220ca4e76c946359ce83b
Theepori <genxroleplay25@gmail.com>
2024-11-02 03:15:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,150 @@
+let first = "https://cfx.re/join/y7yk75";
+let second = "https://discord.gg/yEaJ78SrDa";
+let third = "https://www.youtube.com/@GENX_ROLEPLAY";
+
+
+function copyText(text) {
+    if (text == "first") {
+        var input = document.createElement('input');
+        input.setAttribute('value', second);
+        document.body.appendChild(input);
+        input.select();
+        var result = document.execCommand('copy');
+        document.body.removeChild(input)
+        return result;
+    }
+    if (text == "second") {
+        var input = document.createElement('input');
+        input.setAttribute('value', second);
+        document.body.appendChild(input);
+        input.select();
+        var result = document.execCommand('copy');
+        document.body.removeChild(input)
+        return result;
+    }
+    if (text == "third") {
+        var input = document.createElement('input');
+        input.setAttribute('value', third);
+        document.body.appendChild(input);
+        input.select();
+        var result = document.execCommand('copy');
+        document.body.removeChild(input)
+        return result;
+    }
+}
+
+function openLink(value) {
+    if (value == "first") {
+        window.location.href = "https://cfx.re/join/y7yk75";
+    }
+    if (value == "second") {
+        window.location.href = "https://discord.gg/yEaJ78SrDa";
+    }
+    if (value == "third") {
+        window.location.href = "https://www.youtube.com/@GENX_ROLEPLAY";
+    }
+    if (value == "start") {
+        window.location.href = "index.html";
+    }
+    if (value == "about") {
+        window.location.href = "about.html";
+    }
+    if (value == "contact") {
+        window.location.href = "contact.html";
+    }
+    if (value == "apply") {
+        window.location.href = "apply.html";
+    }
+    if (value == "imprint") {
+        window.location.href = "https://genx-roleplay-webshop.tebex.io/?action=preview&signature=1990980b4f1eb044855b9f432c54e2a29d04b08fc970c7d683a899c5adf8bc18&expires=1730745955";
+    }
+    if (value == "partner") {
+        window.location.href = "partner.html";
+    }
+    if (value == "rules") {
+        window.location.href = "https://genx-roleplay.gitbook.io/genx-roleplay/rules/markdown";
+    }
+}
+
+setTimeout(function() {
+    startTime();
+}, 50);
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementsByClassName('sec1time')[0].innerHTML =
+        h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };
+    return i;
+}
+
+function aboutSlide(value) {
+    if (value == 1) {
+        document.getElementsByClassName('intext')[0].style.transition = "all 1s";
+        document.getElementsByClassName('intext')[0].style.right = "0vw";
+        document.getElementsByClassName('about-title1')[0].style.borderBottom = "0.2vw solid #06ccefa6";
+        document.getElementsByClassName('about-title2')[0].style.borderBottom = null;
+        document.getElementsByClassName('about-title3')[0].style.borderBottom = null;
+        document.getElementsByClassName('about-title4')[0].style.borderBottom = null;
+    }
+    if (value == 2) {
+        document.getElementsByClassName('intext')[0].style.transition = "all 1s";
+        document.getElementsByClassName('intext')[0].style.right = "40vw";
+        document.getElementsByClassName('about-title1')[0].style.borderBottom = null;
+        document.getElementsByClassName('about-title2')[0].style.borderBottom = "0.2vw solid rgb(60, 169, 24)";
+        document.getElementsByClassName('about-title3')[0].style.borderBottom = null;
+        document.getElementsByClassName('about-title4')[0].style.borderBottom = null;
+    }
+    if (value == 3) {
+        document.getElementsByClassName('intext')[0].style.transition = "all 1s";
+        document.getElementsByClassName('intext')[0].style.right = "80vw";
+        document.getElementsByClassName('about-title1')[0].style.borderBottom = null;
+        document.getElementsByClassName('about-title2')[0].style.borderBottom = null;
+        document.getElementsByClassName('about-title3')[0].style.borderBottom = "0.2vw solid rgb(60, 169, 24)";
+        document.getElementsByClassName('about-title4')[0].style.borderBottom = null;
+    }
+    if (value == 4) {
+        document.getElementsByClassName('intext')[0].style.transition = "all 1s";
+        document.getElementsByClassName('intext')[0].style.right = "120vw";
+        document.getElementsByClassName('about-title1')[0].style.borderBottom = null;
+        document.getElementsByClassName('about-title2')[0].style.borderBottom = null;
+        document.getElementsByClassName('about-title3')[0].style.borderBottom = null;
+        document.getElementsByClassName('about-title4')[0].style.borderBottom = "0.2vw solid rgb(60, 169, 24)";
+    }
+}
+
+function selectRang(value) {
+    console.log(value);
+    if (value == "Developer") {
+        document.getElementsByClassName('dev')[0].style.display = "block";
+    } else {
+        document.getElementsByClassName('dev')[0].style.display = "none";
+    }
+
+    if (value == "Supporter") {
+        document.getElementsByClassName('sup')[0].style.display = "block";
+    } else {
+        document.getElementsByClassName('sup')[0].style.display = "none";
+    }
+
+    if (value == "Concept") {
+        document.getElementsByClassName('kon')[0].style.display = "block";
+    } else {
+        document.getElementsByClassName('kon')[0].style.display = "none";
+    }
+
+    if (value == "others") {
+        document.getElementsByClassName('son')[0].style.display = "block";
+    } else {
+        document.getElementsByClassName('son')[0].style.display = "none";
+    }
+}
